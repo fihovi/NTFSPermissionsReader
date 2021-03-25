b9cd78f1b88ec9170d0c1efb75a2e67e6399cda3
Filip Hovorka <filiphovorka@outlook.com>
2021-03-25 15:53:29 +0100

    Added methods to manage CommandLine Arguments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using CommandLine;
 using NTFSPermissions.Lang;
 
@@ -6,78 +8,116 @@ namespace NTFSPermissions
 {
     class Program
     {
+        
+        private static string _scanLocation = "";
+        private static string _exportLocation = "";
+        private static bool _showSystemAccount = false;
+        private static bool _forceScan = false;
+        private static bool _summary = true;
+        private static string _csvFilename = "";
+
         public class Options
         {
+            public Options(string scanLocationPath, string exportLocationPath)
+            {
+                ScanLocationPath = scanLocationPath ?? throw new ArgumentNullException(nameof(scanLocationPath));
+                ExportLocationPath = exportLocationPath ?? throw new ArgumentNullException(nameof(exportLocationPath));
+            }
+
+            public Options(bool verbose = default, bool forceScan = default, bool allowSystemAccounts = default,
+                string csvFilename = null, bool summary = default)
+            {
+                Verbose = verbose;
+                ForceScan = forceScan;
+                AllowSystemAccounts = allowSystemAccounts;
+                CsvFilename = csvFilename;
+                Summary = summary;
+            }
+
+            public Options(bool summary, string csvFilename, string scanLocationPath, string exportLocationPath, bool forceScan, bool allowSystemAccounts)
+            {
+                Summary = summary;
+                CsvFilename = csvFilename;
+                ScanLocationPath = scanLocationPath;
+                ExportLocationPath = exportLocationPath;
+                ForceScan = forceScan;
+                AllowSystemAccounts = allowSystemAccounts;
+            }
+
             [Option('v', "verbose", Required = false, HelpText = @"Set output to verbose messages.")]
-            public bool Verbose { get; set; }
+            public bool Verbose { get; }
 
             [Option('i', "input", Required = true, HelpText = @"ScanLocation")]
-            public string ScanLocationPath { get; set; }
+            public string ScanLocationPath { get; }
 
             [Option('o', "output", Required = true, HelpText = @"Export Location for csv file")]
-            public string ExportLocationPath { get; set; }
+            public string ExportLocationPath { get; }
 
             [Option('f', "force", Required = false, Default = false, HelpText = @"Force skip check for scan")]
-            public bool ForceScan { get; set; }
+            public bool ForceScan { get; }
 
             [Option('s', "systemAccounts", Default = false, Required = false,
                 HelpText = @"Allow to include system Accounts in the report.")]
-            public bool AllowSystemAccounts { get; set; }
+            public bool AllowSystemAccounts { get; }
 
             [Option('p', "prefix", Default = "", Required = false, HelpText = @"Prefix of the report")]
-            public string CsvFilename { get; set; }
+            public string CsvFilename { get; }
 
             [Option('m', "summary", Default = false, Required = false, HelpText = @"Summary on the end of a report")]
-            public bool Summary { get; set; }
+            public bool Summary { get; }
+        }
+
+        private static void RunOptions(Options opts)
+        {
+            Console.WriteLine(opts.Verbose
+                ? $@"Verbose output enabled. Current Arguments: -v {opts.Verbose.ToString()}"
+                : $@"Current Arguments: -v {opts.Verbose.ToString()}");
+            /*Console.WriteLine(opts.Verbose
+                ? $@"Verbose output enabled. Current Arguments: -v {opts.Verbose}"
+                : $@"Current Arguments: -v {opts.Verbose}");*/
+
+            if (opts.ScanLocationPath != "")
+                _scanLocation = opts.ScanLocationPath;
+            else
+                Console.WriteLine(Language_Main.NotDefined, Language_Main.Scan);
+
+            if (opts.ExportLocationPath != "")
+                _exportLocation = opts.ExportLocationPath;
+            else
+                Console.WriteLine(Language_Main.NotDefined, Language_Main.Export);
+
+
+            if (opts.CsvFilename != "")
+                _csvFilename = opts.CsvFilename;
+            _summary = opts.Summary;
+            Console.WriteLine(@"Summary: {0}", _summary.ToString());
+            _forceScan = opts.ForceScan;
+
+            /*Console.WriteLine(opts.AllowSystemAccounts
+                ? $@"System Accounts shown in report -s"
+                : $@"systemAccounts not shown in report -s");*/
+            _showSystemAccount = opts.AllowSystemAccounts;
+            
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            Console.WriteLine(errs);
+            // Environment.Exit(0);
         }
 
         private static void Main(string[] args)
         {
-            string scanLocation = "";
-            string exportLocation = "";
-            bool showSystemAccount = false;
-            bool forceScan = false;
-            bool summary = true;
-            string csvFilename = "";
 
             Parser.Default.ParseArguments<Options>(args)
-                .WithParsed<Options>(o =>
-                {
-                    Console.WriteLine(o.Verbose
-                        ? $@"Verbose output enabled. Current Arguments: -v {o.Verbose.ToString()}"
-                        : $@"Current Arguments: -v {o.Verbose.ToString()}");
-                    /*Console.WriteLine(o.Verbose
-                        ? $@"Verbose output enabled. Current Arguments: -v {o.Verbose}"
-                        : $@"Current Arguments: -v {o.Verbose}");*/
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
 
-                    if (o.ScanLocationPath != "")
-                        scanLocation = o.ScanLocationPath;
-                    else
-                        Console.WriteLine(Language_Main.NotDefined, Language_Main.Scan);
+                Console.WriteLine(Language_Main.ScanLocation, _scanLocation);
+            Console.WriteLine(Language_Main.ExportLocation, _exportLocation);
+            Console.WriteLine(Language_Main.ShowSystemAccount, _showSystemAccount.ToString());
 
-                    if (o.ExportLocationPath != "")
-                        exportLocation = o.ExportLocationPath;
-                    else
-                        Console.WriteLine(Language_Main.NotDefined, Language_Main.Export);
-
-
-                    if (o.CsvFilename != "")
-                        csvFilename = o.CsvFilename;
-                    summary = o.Summary;
-                    Console.WriteLine(@"Summary: {0}", summary.ToString());
-                    forceScan = o.ForceScan;
-
-                    /*Console.WriteLine(o.AllowSystemAccounts
-                        ? $@"System Accounts shown in report -s"
-                        : $@"systemAccounts not shown in report -s");*/
-                    showSystemAccount = o.AllowSystemAccounts;
-                });
-
-            Console.WriteLine(Language_Main.ScanLocation, scanLocation);
-            Console.WriteLine(Language_Main.ExportLocation, exportLocation);
-            Console.WriteLine(Language_Main.ShowSystemAccount, showSystemAccount.ToString());
-
-            if (!forceScan)
+            if (!_forceScan)
             {
                 // ReSharper disable StringLiteralTypo
                 Console.Write(Language_Main.Continue);
@@ -90,12 +130,13 @@ namespace NTFSPermissions
                     case "A":
                         // ReSharper disable StringLiteralTypo
                         Console.WriteLine(@Language_Main.Scanning);
-                        Audit audit = new Audit(scanLocation, exportLocation, "9999999", false, showSystemAccount,
-                            csvFilename, summary);
+                        Audit audit = new Audit(_scanLocation, _exportLocation, "9999999", false, _showSystemAccount,
+                            _csvFilename, _summary);
+                        Console.WriteLine(audit);
                         //TODO FIXME Wait for scan to be done, then exit itself.
                         Console.Write(Language_Main.ScanFinished);
                         Console.ReadKey();
-                        Console.Write(Language_Main.ScanFinished);
+                        Console.WriteLine(Language_Main.Exiting);
                         Environment.Exit(0);
                         break;
                     default:
@@ -109,7 +150,8 @@ namespace NTFSPermissions
             }
             else
             {
-                Audit audit = new Audit(scanLocation, exportLocation, "9999999", false, showSystemAccount, csvFilename, summary);
+                Audit audit = new Audit(_scanLocation, _exportLocation, "9999999", false, _showSystemAccount, _csvFilename, _summary);
+                Console.WriteLine(audit);
                 Console.ReadKey();
                 Console.WriteLine(Language_Main.Exiting);
                 Environment.Exit(0);
